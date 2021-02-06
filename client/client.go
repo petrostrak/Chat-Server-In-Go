18351262e6d6090c17a991ffdee775224b199726
petros <pit.trak@gmail.com>
2021-02-06 11:42:32 +0200

    client added

---
+++ client/client.go
@@ -0,0 +1,15 @@
+package client
+
+import "Chat-Server-In-Go/protocol"
+
+type messageHandler func(string)
+
+type ChatClient interface {
+	Dial(address string) error
+	Start()
+	Close()
+	Send(command interface{}) error
+	SetName(name string) error
+	SendMessage(message string) error
+	Incoming() chan protocol.MessageCommand
+}
