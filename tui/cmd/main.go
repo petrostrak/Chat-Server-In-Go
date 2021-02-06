122d1cfc973a6169934f3142e8d09323c56aa82d
petros <pit.trak@gmail.com>
2021-02-06 12:10:28 +0200

    added tui main

--- tui/cmd/main.go
+++ tui/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"Chat-Server-In-Go/client"
+	"Chat-Server-In-Go/tui"
+	"flag"
+	"log"
+)
 
+func main() {
+	address := flag.String("server", "", "Which server to connect to")
+	flag.Parse()
+
+	client := client.NewClient()
+	err := client.Dial(*address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Close()
+
+	// start the client to listen for incoming message
+	go client.Start()
+
+	tui.StartUi(client)
 }
